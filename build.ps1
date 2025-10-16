7ad36ef57e76c6f3a52c87bdc56cee9c4c6f44a3
pailheres <3245137+pailheres@users.noreply.github.com>
2025-10-16 14:58:38 -0400

    build initial qui marche sur ftm3

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
-$msbuild = "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+$msbuild = "C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe"
 & $msbuild NppMarkdownPanel.sln /target:Clean /target:Build /p:Configuration=Release /p:Platform=x86
 & $msbuild NppMarkdownPanel.sln /target:Clean /target:Build /p:Configuration=Release /p:Platform=x64
